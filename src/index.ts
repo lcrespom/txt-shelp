f7fd76be66a53847ce0e40f740fe368dbe522802
Luis Crespo <lcrespom@gmail.com>
2025-10-21 16:36:23 +0200

    Working on basic history menu

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,11 @@
 import { stdin } from 'node:process'
 import * as readline from 'node:readline'
 
-async function readStdinLines(): Promise<string[]> {
-  const rl = readline.createInterface({
-    input: stdin,
-    crlfDelay: Infinity
-  })
-  const lines: string[] = []
-  for await (const line of rl) lines.push(line)
-  return lines
-}
+// @ts-expect-error - CommonJS module without types
+import { tableMenu } from 'node-terminal-menu'
+// @ts-expect-error - CommonJS module without types
+import keypress from 'keypress'
+import { getHistoryLines } from './history.ts'
 
 function getCommand() {
   return process.argv[2] || 'help'
@@ -19,15 +15,44 @@ function help() {
   console.log('Available commands: help, history')
 }
 
-async function cmdHistory() {
-  if (stdin.isTTY) {
-    console.error('Error: No input provided. Please pipe input to this command.')
-    process.exit(1)
-  }
+async function cmdHistoryStdOut() {
   console.log('Displaying history...')
   console.log('Input lines:')
-  const lines = await readStdinLines()
-  console.log(lines.join('\n'))
+  const lines = getHistoryLines()
+  console.log(lines)
+}
+
+function menuDone(selection: number) {
+  process.stdout.clearScreenDown()
+  console.log('Selection: ' + selection)
+  // showCursor()
+  process.exit(0)
+}
+
+function listenKeyboard(kbHandler: (ch: any, key: any) => void) {
+  process.stdin.setRawMode(true)
+  process.stdin.resume()
+  keypress(process.stdin)
+  process.stdin.on('keypress', kbHandler)
+}
+
+async function cmdHistory() {
+  process.stdout.write('\n')
+  //const items = (await readStdinLines()).join('\n')
+  const items = getHistoryLines()
+  const height = Math.min(process.stdout.rows - 10, items.length)
+  const width = 80 // ToDo compute based on terminal size
+  const menu = tableMenu({
+    items,
+    height: 10,
+    columns: 1,
+    columnWidth: 80,
+    scrollBarCol: width + 1,
+    selection: items.length - 1,
+    //colors: this.initColors(),
+    done: menuDone
+  })
+  listenKeyboard(menu.keyHandler)
 }
 
 function main() {
@@ -37,6 +62,8 @@ function main() {
       return help()
     case 'history':
       return cmdHistory()
+    case 'historyout':
+      return cmdHistoryStdOut()
   }
 }
 
